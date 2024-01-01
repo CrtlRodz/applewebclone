1e6849c3b1cbe54c159ffe62e1e02892d4c5e1a8
Cari-Anne Moore <82580186+CrtlRodz@users.noreply.github.com>
2024-01-01 16:24:26 +0200

    Finishing footers content

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,6 +1,80 @@
-import { Box, Divider, Grid, Link, Typography } from '@mui/material';
+import { Box, Divider, Grid, Link, List, ListItem, Typography } from '@mui/material';
 import "../../styles/footer.css"
 const Footer = () => {
+  const footerData = [
+    {
+      title: "Shop and Learn",
+      items: [
+        { name: "Store", link: "/store" },
+        { name: "Mac", link: "/mac" },
+        { name: "iPad", link: "/ipad" },
+        { name: "iPhone", link: "/iphone" },
+        { name: "Watch", link: "/watch" },
+        { name: "Vision", link: "/vision" },
+        { name: "AirPods", link: "/airpods" },
+        { name: "TV & Home", link: "/tv-home" },
+        { name: "AirTag", link: "/airtag" },
+        { name: "Accessories", link: "/accessories" },
+        { name: "Gift Cards", link: "/gift-cards" }
+      ]
+    },
+    {
+      title: "Entertainment",
+      items: [
+        { name: "Apple One", link: "/apple-one" },
+        { name: "Apple TV+", link: "/apple-tv-plus" },
+        { name: "Apple Music", link: "/apple-music" },
+        { name: "Apple Arcade", link: "/apple-arcade" },
+        { name: "Apple Fitness+", link: "/apple-fitness-plus" },
+        { name: "Apple News+", link: "/apple-news-plus" },
+        { name: "Apple Podcasts", link: "/apple-podcasts" },
+        { name: "Apple Books", link: "/apple-books" },
+        { name: "App Store", link: "/app-store" }
+      ]
+    },
+    {
+      title: "Apple Store",
+      items: [
+        { name: "Find a Store", link: "/find-a-store" },
+        { name: "Genius Bar", link: "/genius-bar" },
+        { name: "Today at Apple", link: "/today-at-apple" },
+        { name: "Apple Camp", link: "/apple-camp" },
+        { name: "Apple Store App", link: "/apple-store-app" },
+        { name: "Certified Refurbished", link: "/certified-refurbished" },
+        { name: "Apple Trade In", link: "/apple-trade-in" },
+        { name: "Financing", link: "/financing" },
+        { name: "Carrier Deals at Apple", link: "/carrier-deals" },
+        { name: "Order Status", link: "/order-status" },
+        { name: "Shopping Help", link: "/shopping-help" },
+      ]
+    },
+
+    {
+      title: "Apple Wallet",
+      items: [
+        { name: "Wallet", link: "/wallet" },
+        { name: "Apple Card", link: "/apple-card" },
+        { name: "Apple Pay", link: "/apple-pay" },
+        { name: "Apple Cash", link: "/apple-cash" },
+      ]
+    },
+
+    {
+      title: "Account",
+      items: [
+        { name: "Manage your Apple Id", link: "/wallet" },
+        { name: "Apple Store Account", link: "/apple-card" },
+        { name: "iCloud.com", link: "/apple-pay" },
+      ]
+    },
+
+
+
+
+
+
+
+  ];
   return (
     <div className="container">
       {/* Paragrahs */}
@@ -15,12 +89,40 @@ const Footer = () => {
         If you reside in the U.S. territories, please call Goldman Sachs at 877-255-5923 with questions about Apple Card.
         </Typography>
         <Typography className='paragraph' paragraph>
-        Learn more about how Apple Card applications are evaluated at support.apple.com/kb/HT209218.
+        Learn more about how Apple Card applications are evaluated at
+        <span>{" "}</span>
+        <Link className='footer-links' href="#" underline="always" >support.apple.com/kb/HT209218.
+        </Link>
         </Typography>
         <Typography className='paragraph' paragraph>
         A subscription is required for Apple TV+.
         </Typography>
         <Divider orientation="horizontal" variant="fullWidth" />
+        <div className="grid-container">
+          {/* list  */}
+          {footerData.map((section, sectionIndex) => (
+  <div className="grid-item" key={sectionIndex}>
+    <div className="section-wrapper">
+      <Typography>{section.title}</Typography>
+      {section.items.map((item, index) => (
+        <List key={index} style={{ padding: 0 }}>
+          <ListItem style={{ padding: '4px 0', margin: 0 }}>
+            <Link className='footer-links' underline='hover' href={item.link} style={{ margin: 0, padding: 0 }}>
+              {item.name}
+            </Link>
+          </ListItem>
+        </List>
+      ))}
+    </div>
+  </div>
+))}
+
+</div>
+        <Typography className='paragraph'>
+        More ways to shop:
+        <Link>Find an Apple Store</Link><span>{" "}</span>or<span>{" "}</span><Link>other retailer</Link><span>{" "}</span>
+        near you. Or call 1-800-MY-APPLE.
+        </Typography>
       </Box>
       {/* List of links and shortcuts */}
       {/* Lower footer */}
